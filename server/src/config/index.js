44ed7e7a6dce7dbcf8b47161e3288f28dc263364
Shah Abdul Ghani <gshahabdul@gmail.com>
2020-12-12 00:45:23 +0530

    used dotenv only in development.

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -2,13 +2,15 @@ import dotenv from "dotenv"
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-const envFound = dotenv.config()
-
-if (envFound.error) {
-  const errMsg = "Could not find .env file"
-  console.error(errMsg)
-  throw new Error(errMsg)
+if (process.env.NODE_ENV === 'development') {
+  const envFound = dotenv.config()  
+  if (envFound.error) {
+    const errMsg = "Could not find .env file"
+    console.error(errMsg)
+    throw new Error(errMsg)
+  }
 }
+
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, PORT } = process.env
 
 const config = {
