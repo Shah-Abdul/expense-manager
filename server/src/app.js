46070a2943545dbc8f68d0da0bf035556ceec546
Shah Abdul Ghani <gshahabdul@gmail.com>
2020-12-12 00:40:20 +0530

    Added a fallback port.

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ const startServer = async () => {
   const app = express()
   await loaders(app)
 
-  app.listen(config.port, () => {
+  app.listen(config.port || 5000, () => {
     console.log("Server listening on " + config.port)
   }).on("error", err => {
     console.error(err)
